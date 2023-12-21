88923ff14bc6d2b07d48620d86a317bf638dbaaf
ferasaljoudi <feras.aljoudi@gmail.com>
2023-12-21 12:39:51 -0600

    Updating Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,6 +5,7 @@ import { FaJava, FaPython, FaReact } from "react-icons/fa";
 import { TiHtml5 } from "react-icons/ti";
 import { BiSolidFileCss } from "react-icons/bi";
 import { AiOutlineConsoleSql } from "react-icons/ai";
+import { FaPhp } from "react-icons/fa6";
 
 
 
@@ -12,20 +13,53 @@ function Skills() {
   return (
     <section className="skills">
       <h2><SiPaloaltosoftware size={25}/> Skills</h2>
+      <p style={{ textAlign: 'center' }}>
+        <a style={{ color: 'black' }} href="https://www.w3schools.com/cpp/" target="_blank" rel="noopener noreferrer">
+          <SiCplusplus size={30} />
+        </a>
+        <a style={{ color: 'black' }} href="https://www.w3schools.com/c/" target="_blank" rel="noopener noreferrer">
+          <SiCodio size={30} /> 
+        </a>
+        <a style={{ color: 'black' }} href="https://www.w3schools.com/java/" target="_blank" rel="noopener noreferrer">
+          <FaJava size={30} />
+        </a>
+        <a style={{ color: 'black' }} href="https://www.w3schools.com/python/" target="_blank" rel="noopener noreferrer">
+          <FaPython size={30} />
+        </a>
+        <a style={{ color: 'black' }} href="https://www.w3schools.com/php/" target="_blank" rel="noopener noreferrer">
+          <FaPhp  size={30} />
+        </a>
+        <a style={{ color: 'black' }} href="https://www.w3schools.com/html/" target="_blank" rel="noopener noreferrer">
+          <TiHtml5 size={30} />
+        </a>
+        <a style={{ color: 'black' }} href="https://www.w3schools.com/css/" target="_blank" rel="noopener noreferrer">
+          <BiSolidFileCss size={30} /> 
+        </a> 
+        <a style={{ color: 'black' }} href="https://react.dev/" target="_blank" rel="noopener noreferrer">
+          <FaReact size={30} />
+        </a>
+        <a style={{ color: 'black' }} href="https://www.w3schools.com/sql/" target="_blank" rel="noopener noreferrer">
+          <AiOutlineConsoleSql size={30} /> 
+        </a> 
+        <a style={{ color: 'black' }} href="https://www.w3schools.com/mysql/" target="_blank" rel="noopener noreferrer">
+          <SiMysql size={33} />
+        </a>
+      </p>
+
       <ul>
         <li><b>Programming Languages:</b>
           <ul style={{ listStyleType: 'square' }}>
-            <li><SiCplusplus size={25} />C++ <SiCodio size={25}/>C <FaJava size={25} />Java <FaPython size={25} />Python</li>
+            <li>C++, C, Java, Python, ARM and MIPS Assembly</li>
           </ul>
         </li>
         <li><b>Web Development:</b>
           <ul style={{ listStyleType: 'square' }}>
-            <li><TiHtml5 size={25} />HTML <BiSolidFileCss size={25} />CSS <FaReact size={25} />React</li>
+            <li>HTML, CSS, React, PHP, WordPress</li>
           </ul>
         </li>
         <li><b>Database Management:</b>
           <ul style={{ listStyleType: 'square' }}>
-            <li><AiOutlineConsoleSql size={25} />SQL <SiMysql size={25} />MySQL</li>
+            <li>SQL, MySQL</li>
           </ul>
         </li>
         <li><b>Software Development:</b>
@@ -33,16 +67,14 @@ function Skills() {
             <li> Agile Methodologies, GitHub</li>
           </ul>
         </li>
-        <li><b>Hardware Design and Simulation:</b>
-          <ul style={{ listStyleType: 'square' }}>
-            <li> VHDL</li>
-          </ul>
-        </li>
+        <li><b>Hardware Design and Simulation:</b> VHDL</li>
+
         <li><b>Development Environments and IDEs:</b>
           <ul style={{ listStyleType: 'square' }}>
-            <li> Visual Studio Code, Codeblocks, Replit, Vivado</li>
+            <li> Visual Studio Code, Codeblocks, Replit, Vivado, Keil uVision</li>
           </ul>
         </li>
+        <li><b>Design and Prototyping:</b> Figma</li>
         <li><b>Modeling programs:</b> Implement the best solution based on the needs by Solid Edge</li>
         
         <li><b>Management Skills:</b> Leveraging expertise in interpersonal communication, team leadership, and strategic business analysis</li>
